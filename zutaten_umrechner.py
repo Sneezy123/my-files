074125f04505d2748d986f873e2557e5fa65412c
Sneezy123 <nilshendrik13@gmail.com>
2022-05-25 22:19:04 +0200

    changed

--- zutaten_umrechner.py
+++ zutaten_umrechner.py
@@ -14,15 +14,15 @@ user_input = input("""Gebe die Zutat ein, die Du umwandeln möchtest?
 (Maximal eine; **Beachte, dass die Maßeinheit mit Wert enthalten ist: z. B.: 10g -> zehn Gramm 
 oder 2 cups -> zwei Tassen (480ml))\n         -->   """)  # asks user
 
-res = reg.search("[0-9]+", user_input)
-gr = res.group()
-wert = int(gr)
+res = reg.search("[0-9]+\.*[0-9]*", user_input)
+wert = float(res.group())
 cups = reg.search("[\s|0-9]*[cC][uU][pP][sS]?", user_input)
 
 if res:
+    print(wert)
     if cups:
         mills = wert * 240
-        print(reg.sub("[0-9]+\s*[cC][uU][pP][sS]?",
+        print(reg.sub("[0-9]+\.*[0-9]*\s*[cC][uU][pP][sS]?",
               str(mills) + " ml", user_input, 1))
 
 

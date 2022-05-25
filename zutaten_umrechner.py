36272d3eabd7f689566b9d7f9060eeb37ceebdd5
Sneezy123 <nilshendrik13@gmail.com>
2022-05-25 21:05:18 +0200

    changed

--- zutaten_umrechner.py
+++ zutaten_umrechner.py
@@ -10,7 +10,9 @@
 # Imports
 import re as reg  # RegEx
 
-user_input = input("Gebe die Zutat ein, die Du umwandeln möchtest? (Maximal eine; **Beachte, dass die Maßeinheit mit Wert enthalten ist: z. B.: 10g -> zehn Gramm oder 2 cups -> zwei Tassen (480ml))\n         -->   ")  # asks user
+user_input = input("""Gebe die Zutat ein, die Du umwandeln möchtest? 
+(Maximal eine; **Beachte, dass die Maßeinheit mit Wert enthalten ist: z. B.: 10g -> zehn Gramm 
+oder 2 cups -> zwei Tassen (480ml))\n         -->   """)  # asks user
 
 
 if reg.search("[0-9]", user_input):
